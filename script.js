a53e2a2c9812626b94651a5bfdd16577677a22d7
MustafaNsrv <86935457+MustafaNasirov@users.noreply.github.com>
2022-07-06 19:13:20 +0400

    changed names to uppercase

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ import { pokemonArray } from './data/pokemon.js'
 
 const cardContainer = document.querySelector(".card-container")
 
-pokemonArray.forEach(pokemon => {
+/* const section = document.createElement("div");
+section.className = "filterSection";
+ */
 
+pokemonArray.forEach(pokemon => {
 let type = pokemon.types.join(' & ')
+let name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
 
     cardContainer.innerHTML += `
     <div class="card">
         <img class="card__image" src="${pokemon.sprite}"></img>
         <div class="card__content">
-            <h1 class="card__heading">${pokemon.name}</h1>
-            <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${type} type pokemon.</p>
+            <h1 class="card__heading">${name}</h1>
+            <p class="card__text">${name} (#${pokemon.id}) is a ${type} type pokemon.</p>
     </div>`
 });
 
