ef8a21a6cf57f6943a7fb4ceed95ed3c65cf507f
MustafaNsrv <86935457+MustafaNasirov@users.noreply.github.com>
2022-07-06 18:50:38 +0400

    fixed pokemon types text

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ import { pokemonArray } from './data/pokemon.js'
 const cardContainer = document.querySelector(".card-container")
 
 pokemonArray.forEach(pokemon => {
+
+let type = pokemon.types.join(' & ')
+
     cardContainer.innerHTML += `
     <div class="card">
         <img class="card__image" src="${pokemon.sprite}"></img>
         <div class="card__content">
             <h1 class="card__heading">${pokemon.name}</h1>
-            <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${pokemon.types} type pokemon.</p>
+            <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${type} type pokemon.</p>
     </div>`
 });
 
